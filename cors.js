78686865d96048ac8d7a1d15ac50ed9cd4547c80
Atle Magnussen <atlemagnussen@gmail.com>
2019-07-30 09:10:55 +0200

    cors added 2

--- cors.js
+++ cors.js
@@ -1,6 +1,6 @@
 class Cors {
     addHeaders(req, res) {
-        let origin = 'http://192.168.1.96';
+        let origin = 'http://localhost:3000';
         try {
             const orgHead = req.get("Origin");
             console.log(orgHead);
@@ -12,6 +12,7 @@ class Cors {
         }
         
         res.set('Access-Control-Allow-Origin', origin);
+        res.set('Access-Control-Allow-Origin', 'http://192.168.1.96:3000');
         res.set("Access-Control-Allow-Credentials", "true");
         res.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
         res.set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
